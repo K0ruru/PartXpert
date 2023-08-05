1a27d4dde23d45caafa9aa384fea051ca20f7445
K0ruru <nurfaizqalbi@gmail.com>
2023-08-05 13:23:18 +0700

    Added more things

--- Script/script.js
+++ Script/script.js
@@ -8,3 +8,20 @@ window.addEventListener("scroll", function () {
 		nav.classList.remove("scrolled-nav");
 	}
 });
+
+function scrollToTop() {
+	window.scrollTo({ top: 0, behavior: "smooth" });
+}
+
+function toggleJumpToTopButton() {
+	const jumpToTopBtn = document.getElementById("jumpToTopBtn");
+	if (window.scrollY > 0) {
+		jumpToTopBtn.style.display = "block"; // Show the button when not at the top
+	} else {
+		jumpToTopBtn.style.display = "none"; // Hide the button when at the top
+	}
+}
+
+window.addEventListener("scroll", toggleJumpToTopButton);
+
+toggleJumpToTopButton();
