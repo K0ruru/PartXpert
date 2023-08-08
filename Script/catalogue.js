6af0b25cbeafe40729eb4b852e61e83b6802d1ad
K0ruru <110281824+K0ruru@users.noreply.github.com>
2023-08-08 10:46:38 +0700

    Updated

---
+++ Script/catalogue.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const categoryDropdown = document.getElementById("category");
+  const productCards = document.querySelectorAll(".product-card");
+
+  categoryDropdown.addEventListener("change", function () {
+    const selectedCategory = categoryDropdown.value;
+
+    productCards.forEach((card) => {
+      if (
+        selectedCategory === "all" ||
+        card.classList.contains(selectedCategory)
+      ) {
+        card.style.display = "block";
+      } else {
+        card.style.display = "none";
+      }
+    });
+  });
+});
+    
--- Script/script.js
+++ Script/script.js
@@ -1,27 +1,60 @@
 window.addEventListener("scroll", function () {
-	const nav = document.getElementById("main-nav");
-	const scrolled = window.scrollY > 80;
+  const nav = document.getElementById("main-nav");
+  const scrolled = window.scrollY > 80;
 
-	if (scrolled) {
-		nav.classList.add("scrolled-nav");
-	} else {
-		nav.classList.remove("scrolled-nav");
-	}
+  if (scrolled) {
+    nav.classList.add("scrolled-nav");
+  } else {
+    nav.classList.remove("scrolled-nav");
+  }
 });
 
 function scrollToTop() {
-	window.scrollTo({ top: 0, behavior: "smooth" });
+  window.scrollTo({ top: 0, behavior: "smooth" });
 }
 
 function toggleJumpToTopButton() {
-	const jumpToTopBtn = document.getElementById("jumpToTopBtn");
-	if (window.scrollY > 0) {
-		jumpToTopBtn.style.display = "block"; // Show the button when not at the top
-	} else {
-		jumpToTopBtn.style.display = "none"; // Hide the button when at the top
-	}
+  const jumpToTopBtn = document.getElementById("jumpToTopBtn");
+  if (window.scrollY > 0) {
+    jumpToTopBtn.style.display = "block"; // Show the button when not at the top
+  } else {
+    jumpToTopBtn.style.display = "none"; // Hide the button when at the top
+  }
 }
 
 window.addEventListener("scroll", toggleJumpToTopButton);
 
-toggleJumpToTopButton();
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+
