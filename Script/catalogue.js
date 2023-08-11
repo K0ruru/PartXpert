10d4d0f3eb4d6f82c2ec513c8bf9a26a0873de56
K0ruru <110281824+K0ruru@users.noreply.github.com>
2023-08-11 08:20:27 +0700

    Temporary Cart

--- Script/catalogue.js
+++ Script/catalogue.js
@@ -1,54 +1,59 @@
 document.addEventListener("DOMContentLoaded", function () {
-	const categoryDropdown = document.getElementById("category");
-	const productCards = document.querySelectorAll(".product-card");
+  const categoryDropdown = document.getElementById("category");
+  const productCards = document.querySelectorAll(".product-card");
 
-	categoryDropdown.addEventListener("change", function () {
-		const selectedCategory = categoryDropdown.value;
+  categoryDropdown.addEventListener("change", function () {
+    const selectedCategory = categoryDropdown.value;
 
-		productCards.forEach((card) => {
-			if (
-				selectedCategory === "all" ||
-				card.classList.contains(selectedCategory)
-			) {
-				card.style.display = "block";
-			} else {
-				card.style.display = "none";
-			}
-		});
-	});
+    productCards.forEach((card) => {
+      if (
+        selectedCategory === "all" ||
+        card.classList.contains(selectedCategory)
+      ) {
+        card.style.display = "block";
+      } else {
+        card.style.display = "none";
+      }
+    });
+  });
 });
 
 function toggleNav() {
-	const sidenav = document.querySelector(".cart");
-	const overlay = document.querySelector(".overlay");
-	if (sidenav.style.width === "450px") {
-		sidenav.style.width = "0";
-		overlay.style.display = "none";
-	} else {
-		sidenav.style.width = "450px";
-		overlay.style.display = "block";
-	}
+  const sidenav = document.querySelector(".cart");
+  const overlay = document.querySelector(".overlay");
+  if (sidenav.style.width === "450px") {
+    sidenav.style.width = "0";
+    overlay.style.display = "none";
+  } else {
+    sidenav.style.width = "450px";
+    overlay.style.display = "block";
+  }
 }
 function openNav() {
-	const sidenav = document.querySelector(".cart");
-	const overlay = document.querySelector(".overlay");
-	sidenav.style.width = "450px";
-	overlay.style.display = "block";
+  const sidenav = document.querySelector(".cart");
+  const overlay = document.querySelector(".overlay");
+  sidenav.style.width = "450px";
+  overlay.style.display = "block";
 }
 
 function closeNav() {
-	const sidenav = document.querySelector(".cart");
-	const overlay = document.querySelector(".overlay");
-	sidenav.style.width = "0";
-	overlay.style.display = "none";
+  const sidenav = document.querySelector(".cart");
+  const overlay = document.querySelector(".overlay");
+  sidenav.style.width = "0";
+  overlay.style.display = "none";
 }
 
 document.addEventListener("click", function (event) {
-	const sidenav = document.querySelector(".cart");
-	const toggleButton = document.querySelector(".toggle-btn");
-	const targetElement = event.target;
+  const sidenav = document.querySelectorAll(".cart");
+  const toggleButton = document.querySelector(".toggle-btn");
+  const removeBtn = document.querySelector(".remove-button");
+  const targetElement = event.target;
 
-	if (targetElement !== sidenav && targetElement !== toggleButton) {
-		closeNav();
-	}
+  if (
+    targetElement !== sidenav &&
+    targetElement !== toggleButton &&
+    targetElement !== removeBtn
+  ) {
+    closeNav();
+  }
 });
--- Script/product.js
+++ Script/product.js
@@ -1,78 +1,78 @@
 // Sample hardcoded product data
 const products = [
-	{
-		id: 1,
-		name: "Ryzen 9 5950X",
-		price: "Rp. 6.000.000,00",
-		imageSrc: "/images/Amd Ryzen 9.png",
-		description:
-			"The Ryzen 9 5950X is a high-performance CPU engineered by AMD to deliver exceptional computing power. With its cutting-edge architecture, this processor boasts an impressive 16 cores and 32 threads, providing unparalleled multitasking capabilities and fluid performance. Whether you're a creative professional requiring raw processing power or a dedicated gamer seeking seamless gameplay, the Ryzen 9 5950X stands as an ideal choice. Its advanced technology optimizes power efficiency while maintaining impressive clock speeds, ensuring that even the most demanding tasks are executed effortlessly. Experience the future of computing with the Ryzen 9 5950X.",
-		specifications: [
-			"Cores: 16",
-			"Threads: 32",
-			"Base Clock: 3.4 GHz",
-			"Max Boost Clock: 4.9 GHz",
-		],
-	},
-	{
-		id: 2,
-		name: "ASUS RTX 4090",
-		price: "Rp. 6.000.000,00",
-		imageSrc: "/images/RTX 4090.png",
-		description:
-			"The RTX 4090 is anticipated to be NVIDIA's flagship graphics card, representing the pinnacle of cutting-edge gaming and rendering performance. With advancements in technology and architecture, the RTX 4090 is expected to deliver unprecedented levels of realism, speed, and efficiency for gamers, content creators, and professionals alike.",
-		specifications: [
-			"Architecture: Next-generation NVIDIA architecture",
-			"CUDA Cores: Increased number for enhanced processing power",
-			"Ray Tracing Cores: More ray tracing cores for improved real-time ray tracing",
-			"Tensor Cores: Enhanced AI capabilities for improved performance and features",
-			"Memory: Larger and faster GDDR6X or HBM memory",
-			"Memory Interface: Wider memory interface for improved bandwidth",
-			"Clock Speeds: Higher base and boost clock speeds",
-		],
-	},
+  {
+    id: 1,
+    name: "Ryzen 9 5950X",
+    price: "Rp. 6.000.000,00",
+    imageSrc: "/images/Amd Ryzen 9.png",
+    description:
+      "The Ryzen 9 5950X is a high-performance CPU engineered by AMD to deliver exceptional computing power. With its cutting-edge architecture, this processor boasts an impressive 16 cores and 32 threads, providing unparalleled multitasking capabilities and fluid performance. Whether you're a creative professional requiring raw processing power or a dedicated gamer seeking seamless gameplay, the Ryzen 9 5950X stands as an ideal choice. Its advanced technology optimizes power efficiency while maintaining impressive clock speeds, ensuring that even the most demanding tasks are executed effortlessly. Experience the future of computing with the Ryzen 9 5950X.",
+    specifications: [
+      "Cores: 16",
+      "Threads: 32",
+      "Base Clock: 3.4 GHz",
+      "Max Boost Clock: 4.9 GHz",
+    ],
+  },
+  {
+    id: 2,
+    name: "ASUS RTX 4090",
+    price: "Rp. 12.000.000,00",
+    imageSrc: "/images/RTX 4090.png",
+    description:
+      "The RTX 4090 is anticipated to be NVIDIA's flagship graphics card, representing the pinnacle of cutting-edge gaming and rendering performance. With advancements in technology and architecture, the RTX 4090 is expected to deliver unprecedented levels of realism, speed, and efficiency for gamers, content creators, and professionals alike.",
+    specifications: [
+      "Architecture: Next-generation NVIDIA architecture",
+      "CUDA Cores: Increased number for enhanced processing power",
+      "Ray Tracing Cores: More ray tracing cores for improved real-time ray tracing",
+      "Tensor Cores: Enhanced AI capabilities for improved performance and features",
+      "Memory: Larger and faster GDDR6X or HBM memory",
+      "Memory Interface: Wider memory interface for improved bandwidth",
+      "Clock Speeds: Higher base and boost clock speeds",
+    ],
+  },
 ];
 
 // Function to get the selected product ID from query parameter
 function getProductIdFromUrl() {
-	const params = new URLSearchParams(window.location.search);
-	return params.get("productId");
+  const params = new URLSearchParams(window.location.search);
+  return params.get("productId");
 }
 
 // Function to populate product details on the page
 function populateProductDetails(product) {
-	const productImage = document.getElementById("product-image");
-	const productName = document.getElementById("product-name");
-	const productPrice = document.getElementById("product-price");
-	const specificationsList = document.getElementById("product-specifications");
-	const productDescription = document.getElementById("product-description");
+  const productImage = document.getElementById("product-image");
+  const productName = document.getElementById("product-name");
+  const productPrice = document.getElementById("product-price");
+  const specificationsList = document.getElementById("product-specifications");
+  const productDescription = document.getElementById("product-description");
 
-	productImage.src = product.imageSrc;
-	productName.textContent = product.name;
-	productPrice.textContent = product.price;
+  productImage.src = product.imageSrc;
+  productName.textContent = product.name;
+  productPrice.textContent = product.price;
 
-	const descriptionParagraph = document.createElement("p");
-	descriptionParagraph.innerHTML = product.description;
-	productDescription.appendChild(descriptionParagraph);
+  const descriptionParagraph = document.createElement("p");
+  descriptionParagraph.innerHTML = product.description;
+  productDescription.appendChild(descriptionParagraph);
 
-	product.specifications.forEach((spec) => {
-		const specItem = document.createElement("li");
-		specItem.textContent = spec;
-		specificationsList.appendChild(specItem);
-	});
+  product.specifications.forEach((spec) => {
+    const specItem = document.createElement("li");
+    specItem.textContent = spec;
+    specificationsList.appendChild(specItem);
+  });
 }
 
 // Page initialization
 window.addEventListener("DOMContentLoaded", () => {
-	const productId = getProductIdFromUrl();
-	const selectedProduct = products.find(
-		(product) => product.id === parseInt(productId)
-	);
+  const productId = getProductIdFromUrl();
+  const selectedProduct = products.find(
+    (product) => product.id === parseInt(productId)
+  );
 
-	if (selectedProduct) {
-		populateProductDetails(selectedProduct);
-	} else {
-		// Handle case where product is not found
-		console.log("Product not found");
-	}
+  if (selectedProduct) {
+    populateProductDetails(selectedProduct);
+  } else {
+    // Handle case where product is not found
+    console.log("Product not found");
+  }
 });
